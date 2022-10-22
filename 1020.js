b5ed33dae40ab28a8161b19810b5748cfa629959
Eric da Costa <ericdacosta72@gmail.com>
2022-10-22 10:17:45 -0300

    muda valor da variavel input]

--- 1020.js
+++ 1020.js
@@ -1,20 +1,18 @@
 function main(){
-    var input = require('fs').readFileSync('./stdin', 'utf8');
+    var input = require('fs').readFileSync('/dev/stdin', 'utf8');
     var lines = input.split('\n');
 
     let ano, mes;
     let idadeDias = parseInt(lines.shift());
-    ano = parseInt(idadeDias / 365); /* Anos */
+    ano = parseInt(idadeDias / 365);
     idadeDias = idadeDias % 365;
-    mes = parseInt(idadeDias / 30); /* Meses */
+    mes = parseInt(idadeDias / 30);
     idadeDias = idadeDias % 30;
 
 
 let resultado = `${ano} ano(s)
 ${mes} mes(es)
 ${idadeDias} dia(s)`;
-
-/* Final */
 
 console.log(resultado);
 }
